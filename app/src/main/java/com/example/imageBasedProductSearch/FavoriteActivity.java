b8a58785e871a31f80be1078f97d5f8f67befda2
zartasha12 <zartasha.ehsan123@gmail.com>
2021-05-09 02:48:39 +0500

    lots of changes

---
+++ app/src/main/java/com/example/imageBasedProductSearch/FavoriteActivity.java
@@ -0,0 +1,13 @@
+package com.example.imageBasedProductSearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FavoriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite);
+    }
+}
